a87833c471f8035bc55b79914353aed3433bd6cc
ArthurPPPPP <92211379+ArthurPPPPP@users.noreply.github.com>
2021-11-22 16:46:06 +0200

    feature/news-screen

---
+++ src/pages/News/News.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { loadNews } from "../../api/news";
+
+const News = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [News, setNews] = useState([]);
+  useEffect(() => {
+    loadNews().then(
+      (res) => {
+        setIsLoaded(true);
+        setNews(res);
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    );
+  }, []);
+  if (error) {
+    console.log("error");
+  } else if (!isLoaded) {
+    console.log("Loading");
+  } else {
+    console.log(News);
+  }
+
+  return <div></div>;
+};
+export default News;
