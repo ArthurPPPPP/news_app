a8adead3b76267b9f12f49ab9e3ed3384ecc6e35
ArthurPPPPP <92211379+ArthurPPPPP@users.noreply.github.com>
2021-12-08 17:01:22 +0200

    Raw version of the serch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { News } from "./pages/News";
 
 function App() {
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      
+      <div className="logo">
+        <h2>News App</h2>
+      </div>
+    </header>
+  );
+};
